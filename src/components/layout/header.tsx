4f5723410088bdd6a1aaf6af8350ada35d177c02
Romel Mori <romelmori81@gmail.com>
2025-10-14 20:48:45 +0000

    Runtime Error
    Error: React is not defined
    
    src/components/layout/heade

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
